cd4ff7117ef66190d8c8d850a4ef9fac15f25ebb
Daniel Moran <damg75cd@gmail.com>
2021-05-11 00:38:46 -0400

    sitio web completando la estructura

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,7 +10,7 @@ class PostController extends Controller
     public function index () 
     {
         return view ('index', [
-            'posts' => Post::latest()->paginate()
+            'posts' => Post::latest()->paginate() //pagination
         ]);
     }
 }
