c72c0b6ee284864248b070efd4f7256d85c4087a
Daniel Moran <damg75cd@gmail.com>
2021-05-11 01:58:41 -0400

    config inital v2

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\V1\PostController as PostV1;
+use App\Http\Controllers\Api\V2\PostController as PostV2;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -14,7 +17,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::apiResource('v1/posts', App\Http\Controllers\Api\V1\PostController::class)
+Route::apiResource('v1/posts', PostV1::class)
+->only(['show', 'index', 'destroy']);
+
+Route::apiResource('v2/posts', PostV2::class)
 ->only(['show', 'index']);
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
