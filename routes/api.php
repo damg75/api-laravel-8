b5091b02247840a68adad05a48a5bf153dc9b545
Daniel Moran <damg75cd@gmail.com>
2021-05-11 02:33:01 -0400

    intro a privatizacion

--- routes/api.php
+++ routes/api.php
@@ -18,10 +18,19 @@ use App\Http\Controllers\Api\V2\PostController as PostV2;
 */
 
 Route::apiResource('v1/posts', PostV1::class)
-->only(['show', 'index', 'destroy']);
+    ->only(['show', 'index', 'destroy'])
+    ->middleware('auth:sanctum');
+
 
 Route::apiResource('v2/posts', PostV2::class)
-->only(['show', 'index']);
+    ->only(['show', 'index'])
+    ->middleware('auth:sanctum');
+
+
+
+
+
+
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
